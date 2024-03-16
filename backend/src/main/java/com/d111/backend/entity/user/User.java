4fdf83d7a3dfb7507ade397d11dfb7680ceb9132
김준수 <kia54682@gmail.com>
2024-03-16 17:44:10 +0900

    Feat: Comment Entity 추가

--- backend/src/main/java/com/d111/backend/entity/user/User.java
+++ backend/src/main/java/com/d111/backend/entity/user/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53487d9e6e461ce33491f4bd01d69bacfa256cfe01e7eac07929b42d3a3f8d78
-size 1516
+oid sha256:ee07227f4edc03ebcdb4fcf63362ba2644cd835a1d7e71ff05cfb6f31ff1673c
+size 1588
