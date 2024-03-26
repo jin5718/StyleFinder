786c94ea03ac650a9c874db488c0258126f7f111
37jsbean <37jsbean@naver.com>
2024-03-26 16:33:07 +0900

    Fix: Docker-compose 파일 수정

--- frontend/src/pages/FeedDetail.tsx
+++ frontend/src/pages/FeedDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42a6ff7ba9ef2ef4449f1349a0ec8d26b301c4c434ce6a6998d8a4f690b650df
-size 2973
+oid sha256:4406928282a313ea4a8590c01410167bbf302fe97007b2d1f428a93bec329f58
+size 2902
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce1045cf7a4066b651b52d61edfad7a0de34cdb04309bbee14d093266cb4e0d1
-size 6559
+oid sha256:c2166ffd77e7b0dca0dbccae1aef151c50bad9d4f8ba127fcbd2da8fede0f2c7
+size 5950
