3b0980fc98f1e394e31e719337420a6d7b2ef8e2
정은진 <jin5718@naver.com>
2024-03-26 10:35:18 +0900

    Feat: Feed feedThumbnail 반환타입 수정

--- backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9640484ce9e8771d7f7d2c6a41e8d5a479a30aa6e89d8814c13bc79d3a13579
-size 11538
+oid sha256:b548bf97db6d1a7aa254fffdb4e27499ba973100fb566c521b7b24cca3adff7f
+size 13381
