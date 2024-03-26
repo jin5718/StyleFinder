815d00837a1e947c7fb05d045d4678d972ee24ab
37jsbean <37jsbean@naver.com>
2024-03-26 10:58:33 +0900

    Fix: Typescript 오류 수정

--- frontend/src/pages/FeedDetail.tsx
+++ frontend/src/pages/FeedDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d04abcd112efb6cb8530691b3ac468f2a5c88720c7d1b35b64189233635f915f
-size 2851
+oid sha256:1ed895b046d25269abf7389c129f0a022d9f7a4a76ab2a2800134b97aab6d2a3
+size 2973
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a2cd9bbf5ae7c5cb12745cca76194bd8cfec03c39dd03cca56cf04eae96c4a
-size 3709
+oid sha256:b79136f759ed7e4d1ed7394ba7d406760600a4ba82fa5039a4a3eaf495501ac0
+size 3313
