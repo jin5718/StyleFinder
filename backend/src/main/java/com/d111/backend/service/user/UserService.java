aa79cfd2d4d98d5e84cba26c91787fe8eb8cd49e
김준수 <kia54682@gmail.com>
2024-03-27 14:25:00 +0900

    Feat: User entity & updateUserInfo 수정

--- backend/src/main/java/com/d111/backend/service/user/UserService.java
+++ backend/src/main/java/com/d111/backend/service/user/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeb0be90952c9355e55bd21c6f9d3cd6765bcaedc39577c404a8ba8612e637e1
-size 1101
+oid sha256:8375b1c494abef303ba5d9ef957a36c9f6dc3b99dacc4bc22122f6024402ed90
+size 1082
--- backend/src/main/java/com/d111/backend/serviceImpl/user/UserServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/user/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d03c8ea34a8e1e5c0651e01161b7cef9719e30eceeed9c9df5e0d9d90cefa374
-size 11943
+oid sha256:36e41e75f4067884d7d48cc3cb7aad2b45d6ba8924e4663f67773b2a266b0be7
+size 11527
