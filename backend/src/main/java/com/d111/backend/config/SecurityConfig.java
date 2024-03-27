a6c3f3fbf0f9b0fb96e6d70108c8cc6e0c042c1e
김준수 <kia54682@gmail.com>
2024-03-27 14:12:07 +0900

    Fix: Cors resolve

--- backend/src/main/java/com/d111/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/d111/backend/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53b6df74bb2405b74cde9fc7555f38bdf320a0403e7381620efa51993a07f9a6
-size 3705
+oid sha256:a3fc2a44cb7de72f2fdc717216fbb7767bc1b76adef0c34a5f426cf8e75841e8
+size 3716
--- backend/src/main/java/com/d111/backend/serviceImpl/user/UserServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/user/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb8063fc6e64dab9ed5f1ce86087b7f1e3e287d5e1c7ea2bb0e2b43d293028a
-size 11942
+oid sha256:d03c8ea34a8e1e5c0651e01161b7cef9719e30eceeed9c9df5e0d9d90cefa374
+size 11943
