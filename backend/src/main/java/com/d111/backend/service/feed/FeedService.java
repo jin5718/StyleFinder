154bff9b013bd43302023995afd9c40a45c6535c
정은진 <jin5718@naver.com>
2024-03-27 10:34:45 +0900

    Feat: yml 파일 수정

--- backend/src/main/java/com/d111/backend/service/feed/FeedService.java
+++ backend/src/main/java/com/d111/backend/service/feed/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1db0a08d35ee7f8827cda0dedca6d85eb35b516524a4ab44e89098fe1a8852b5
-size 1233
+oid sha256:370405583b4f4a3b40c92f785b555e14123ee8294aeea6ce6a5d1631cc90755f
+size 1312
