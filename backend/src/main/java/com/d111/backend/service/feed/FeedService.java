096eba43cb9a1129909d7a4cf481b482e8fa038c
정은진 <jin5718@naver.com>
2024-03-28 09:38:18 +0900

    Feat: Fitting 추가

--- backend/src/main/java/com/d111/backend/service/feed/FeedService.java
+++ backend/src/main/java/com/d111/backend/service/feed/FeedService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:370405583b4f4a3b40c92f785b555e14123ee8294aeea6ce6a5d1631cc90755f
-size 1312
+oid sha256:2848d27bbe10edf37c9a4d016f66bcd3fb6783913f8c6dc0e19936087a45ce01
+size 1431
--- backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2197df98bdc1db7cd75b80bcf9f22e7260e39ded60c0abe98de870db42af6403
-size 14712
+oid sha256:75c1f502ef7d9e622ed820c3d3e531cc77783f32c0a7b9af66cbb8733eba1dec
+size 16286
