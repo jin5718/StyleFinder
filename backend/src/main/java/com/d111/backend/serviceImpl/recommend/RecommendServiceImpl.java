3e1ce53b4d067f02afae3ba86a25ecee006d6d97
정은진 <jin5718@naver.com>
2024-03-30 00:41:27 +0900

    Feat: RecommendServiceImpl 수정

--- backend/src/main/java/com/d111/backend/serviceImpl/recommend/RecommendServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/recommend/RecommendServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9392bb5640f0ab2ff345001eaea6a069517ff4a3b039ee2cbbc0d89cf73879a
-size 4729
+oid sha256:3211135ea24a521463b0644e4a892f291c9f1d08f9a91ed3954d2cd48fd71bdd
+size 4642
