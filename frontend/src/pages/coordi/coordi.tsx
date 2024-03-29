0ba65072a4201a318dbb931b65f58902b5530987
37jsbean <37jsbean@naver.com>
2024-03-29 13:53:23 +0900

    Fix: VITE API BASE URL 8081 변경

--- frontend/src/pages/coordi/coordi.tsx
+++ frontend/src/pages/coordi/coordi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b49d44ecdd2385ae3dca48782c3d91c08284ad5c9dbad8bc270532b486a6e512
-size 14140
+oid sha256:d9e6cde37b2bcdfa62ac4a3f7346b2e107400238bebc91188db304e7aa216565
+size 14088
