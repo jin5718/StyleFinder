4ad16ea571865bccdbdd13b74da93d9585286706
37jsbean <37jsbean@naver.com>
2024-03-29 13:19:19 +0900

    Fix: FE 일부 미사용 코드 수정

--- frontend/src/pages/coordi/coordi.tsx
+++ frontend/src/pages/coordi/coordi.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57c100dd95841a1dd0c5f5ab88d15f4828eca38b2c3cfe7296d60af51a134c91
-size 14672
+oid sha256:b49d44ecdd2385ae3dca48782c3d91c08284ad5c9dbad8bc270532b486a6e512
+size 14140
