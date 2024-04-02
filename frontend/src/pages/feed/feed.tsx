fd4cc15e4bded38701fa789842f3abbef152c5fe
오승현 <ohsh9529@gmail.com>
2024-04-02 17:48:09 +0900

    feed & feeddetail css수정

--- frontend/src/pages/feed/feed.tsx
+++ frontend/src/pages/feed/feed.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3838b8dd49247dee0b70f3db8b20c46b2150eb5976971a38f6c2837bcb044a43
-size 12419
+oid sha256:0b191c24fdec4215c7c6800271dd3b44f5892ca9358c193c2a82ea861e70fc12
+size 12427
--- frontend/src/pages/feed/feedDetail.tsx
+++ frontend/src/pages/feed/feedDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797c1a89c05625c229b02207e029a2aaeb49d71e8f55e128a8a30568a76bad23
-size 9423
+oid sha256:939315e826036ff8062e5f604f3cde18ab01de004c84b84b1810b37460c5bad1
+size 9420
