aae7e79441c2d590b2a40ab1b51756bbd0b971bf
오승현 <ohsh9529@gmail.com>
2024-04-02 20:22:19 +0900

    마이페이지 css수정

--- frontend/src/pages/my/my.tsx
+++ frontend/src/pages/my/my.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:061e5ea673be80de48e78d1f605582e83b9123d6e37c0f745f12642aeefd6e81
-size 12795
+oid sha256:0c6c4f9e2b763f05d08c31782f632c363f8fbf832b8372087a7f59485f339fae
+size 13379
