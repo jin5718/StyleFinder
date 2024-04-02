e971cf7acd32f5bb6e0d764a158d18161b47c849
오승현 <ohsh9529@gmail.com>
2024-04-02 14:22:02 +0900

    feed 수정

--- frontend/src/pages/feed/feedDetail.tsx
+++ frontend/src/pages/feed/feedDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d97ec6e4e92488fcd0c19a04f2e462f706fab3703ac8a70f8bedf526b09fb85c
-size 5336
+oid sha256:418bc625a0ebc7cef834d8137296c001660b7e0a32899f22572c1806661e9d0b
+size 6687
