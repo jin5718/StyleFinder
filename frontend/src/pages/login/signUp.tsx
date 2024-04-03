e4e7693273bac34ca0928c7bae4b3c5f58cef24b
37jsbean <37jsbean@naver.com>
2024-04-03 19:00:42 +0900

    Fix: Build 오류 수정

--- frontend/src/pages/login/signUp.tsx
+++ frontend/src/pages/login/signUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c83bc5fb3f44cf549d8d0788112c55c199e4686307d794261ccd8251d6554db1
+oid sha256:4a709de9678b01fa13a971c1b024945d3d8f5ada4b5edd99451e80cd29656117
 size 9769
