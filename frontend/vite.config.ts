667c0364e29c271068bebb611601adc4cbbdd752
37jsbean <37jsbean@naver.com>
2024-04-03 18:46:43 +0900

    Feat: svg plugin 설치

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca1addd70ada0abacd5505d8704f5db887ffe652501c5b46b13b1ff1d2016d1f
-size 222
+oid sha256:9dd376d659e9ddff37bc01a5722db5f0c2f582315f759139137577c58d84e46d
+size 366
