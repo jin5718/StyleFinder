66bad9bb34ff4e1619c4e1f35b3b65ef2a25a5a6
김준수 <kia54682@gmail.com>
2024-04-03 17:39:45 +0900

    Fix: closet create alert 추가

--- backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
+++ backend/src/main/java/com/d111/backend/serviceImpl/feed/FeedServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6aa375ce229978b9b96d17639660ae84ebd9b63f3866686d295a25d71327401
-size 28643
+oid sha256:62f473fddfc8d94ae184a9c53af6f4043be59dcdd3c4e48d415aacece89bb758
+size 28680
