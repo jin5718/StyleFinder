919fcbb6e11b700c9361478cb4c6e922ed190f39
김준수 <kia54682@gmail.com>
2024-04-04 08:54:06 +0900

    Fix: Fix bug

---
+++ exec/DB_dumpdata/mysql_dumpdata.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0df9b624ccb6b470ef2f696bf0494d5bbedc520b8942c1b14de8e17428917d10
+size 25896
--- frontend/src/pages/login/signUp.tsx
+++ frontend/src/pages/login/signUp.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:767b3fc7f1bccfe174f4c71c2521a78afe56b158c87a0e2ccb83e25afe0dd6f2
-size 9856
+oid sha256:db408ce66379c337156ccb4c10ff339147457d173b9a08473578b9a2e93001cb
+size 9844
