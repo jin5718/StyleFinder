d43879073af7326b0c6eb83b88b3f5454264794e
김준수 <kia54682@gmail.com>
2024-04-03 22:25:45 +0900

    Feat: 내 옷장 삭제 기능 구현

--- frontend/src/pages/closet/my-closet.tsx
+++ frontend/src/pages/closet/my-closet.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:001f00f0a7ebe7f5d545c81fd022b462f1539f5cb84d32abda72796152597730
-size 3926
+oid sha256:59b3dc58eec093b27eaf6ec5dcacf22fb0d8c8e5c1d2c7e67436998f3fb9bb78
+size 3875
