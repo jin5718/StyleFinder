fa9053621dd2ecc1c9feed87897acd8eea3c0bfb
37jsbean <37jsbean@naver.com>
2024-04-04 07:48:46 +0900

    Fix: Error 수정

--- frontend/src/pages/my/my.tsx
+++ frontend/src/pages/my/my.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27130671e0db58000fcf6fb923c337ff9dafb385101a94f9ccd3a82012003af7
-size 13631
+oid sha256:60d44ef4b26bddc65f348d6f2f1a5da68c5e44ed5cb27935576c545f16e379b5
+size 13576
